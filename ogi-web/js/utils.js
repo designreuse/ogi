63b3f64d810f3dda9ff09b56e0c50fe39749c8b6
jerep6 <jerep6@gmail.com>
2014-01-12 10:54:19 +0100

    Add round method to number

--- ogi-web/js/utils.js
+++ ogi-web/js/utils.js
@@ -1,3 +1,9 @@
 function fctName(fct) {
     return /\W*function\s+([\w\$]+)\(/.exec(fct.toString())[1]
 }
+
+
+// Extends javascript number
+Number.prototype.round = function(places) {
+    return +(Math.round(this + "e+" + places)  + "e-" + places);
+}
