7a67d3641ed60ae043db2cd07af22f4757f35c2d
jerep6 <jerep6@gmail.com>
2014-01-12 11:04:06 +0100

    Jasper reports

--- ogi-parent/ogi-services/src/main/java/fr/jerep6/ogi/service/impl/ServiceReportImpl.java
+++ ogi-parent/ogi-services/src/main/java/fr/jerep6/ogi/service/impl/ServiceReportImpl.java
@@ -1,19 +1,18 @@
 package fr.jerep6.ogi.service.impl;
 
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
+import java.io.FileInputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
 import net.sf.jasperreports.engine.JRAbstractExporter;
-import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
 import net.sf.jasperreports.engine.util.JRLoader;
@@ -21,6 +20,7 @@ import net.sf.jasperreports.engine.util.JRLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.google.common.base.Preconditions;
@@ -29,16 +29,17 @@ import com.google.common.base.Strings;
 import fr.jerep6.ogi.exception.technical.JasperException;
 import fr.jerep6.ogi.framework.exception.TechnicalException;
 import fr.jerep6.ogi.framework.service.impl.AbstractService;
-import fr.jerep6.ogi.persistance.bo.RealProperty;
-import fr.jerep6.ogi.service.ServiceRealProperty;
 import fr.jerep6.ogi.service.ServiceReport;
 
 @Service("serviceReport")
 public class ServiceReportImpl extends AbstractService implements ServiceReport {
-	private final Logger		LOGGER	= LoggerFactory.getLogger(ServiceReportImpl.class);
+	private final Logger	LOGGER	= LoggerFactory.getLogger(ServiceReportImpl.class);
 
 	@Autowired
-	private ServiceRealProperty	serviceRealProperty;
+	private DataSource		dataSource;
+
+	@Value("${jasper.directory.root}")
+	private String			jasperRootDirectory;
 
 	private ByteArrayOutputStream generate(JasperPrint print, String format) throws JRException {
 		Preconditions.checkArgument(!Strings.isNullOrEmpty(format));
@@ -66,23 +67,19 @@ public class ServiceReportImpl extends AbstractService implements ServiceReport 
 		Preconditions.checkArgument(!Strings.isNullOrEmpty(prpReference));
 
 		try {
-			JasperReport report = (JasperReport) JRLoader.loadObject(getClass().getResourceAsStream("/report1.jasper"));
-
-			RealProperty readByReference = serviceRealProperty.readByReference(prpReference);
-			List<RealProperty> l = new ArrayList<>();
-			l.add(readByReference);
+			JasperReport report = (JasperReport) JRLoader.loadObject(new FileInputStream(jasperRootDirectory
+					+ "/report1.jasper"));
 
 			Map<String, Object> parameters = new HashMap<String, Object>();
-			parameters.put("criteres", "test de critere");
+			parameters.put("ROOT_DIR", jasperRootDirectory);
+			parameters.put("reference", prpReference);
 
-			JRDataSource dataSource = new JRBeanCollectionDataSource(l);
-			JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
+			JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource.getConnection());
 
 			return generate(print, format);
 		} catch (Exception e) {
-			LOGGER.error("Error generating report for property" + prpReference, e);
+			LOGGER.error("Error generating report for property " + prpReference, e);
 			throw new JasperException(e.getMessage(), e);
 		}
 	}
-
 }
