c76eee2d28003dabaea889cb7d0f58925c49514b
jerep6 <jerep6@gmail.com>
2014-02-09 11:23:21 +0100

    Delete property on acimflo

--- ogi-web/js/controller/property/ControllerPrpTabPartner.js
+++ ogi-web/js/controller/property/ControllerPrpTabPartner.js
@@ -1,4 +1,4 @@
-function ControllerPrpTabPartner($scope, Page, $routeParams, ServiceConfiguration, ServiceObject, ServiceAlert, $http, $log, Utils) {
+function ControllerPrpTabPartner($scope, Page, $routeParams, ServiceConfiguration, ServiceAlert, $http, $log, Utils) {
 
     $scope.partners = [];
     $scope.partners.push({"name":"acimflo", "img":"img/LogoAcimflo.png", "exist": null});
@@ -18,7 +18,51 @@ function ControllerPrpTabPartner($scope, Page, $routeParams, ServiceConfiguratio
         var references = [$scope.prp.reference];
         $http.post(ServiceConfiguration.API_URL+"/rest/synchronisation/"+partner+"/", references)
             .success(function (data, status) {
+                var errors = data.filter(function(elt) {return elt.code != "OK"});
 
+                // Si au moins un retour en erreur, afficher les msg d'erreurs
+                if(errors.length > 0 ) {
+                    var msg = "Des erreurs se sont produites lors du traitement de synchrosation : <br />";
+                    errors.forEach(function(elt) {
+                        msg += elt.message +" <br />";
+                    });
+                    ServiceAlert.addError(msg);
+                }
+                // Si tous les retours sont en succès => 1 seul message OK
+                else {
+                    ServiceAlert.addSuccess("Synchronisation réalisée avec succès");
+                    // Le bien est maintenant synchronisé. Dans tous les cas, il est présent dans le système du partenaire
+                    // Positionne donc le boolean "exist" à true
+                    $scope.partners.filter(function(elt) {return elt.name == partner}).forEach(function(elt) {
+                        elt.exist = true;
+                    })
+                }
+            });
+    }
+
+    $scope.delete = function(partner) {
+        var references = [$scope.prp.reference];
+        $http.delete(ServiceConfiguration.API_URL+"/rest/synchronisation/"+partner+"/", { "params" : {"ref" :references}})
+            .success(function (data, status) {
+                // Extract errors results
+                var errors = data.filter(function(elt) {return elt.code != "OK"});
+
+                // Si au moins un retour en erreur, afficher les msg d'erreurs
+                if(errors.length > 0 ) {
+                    var msg = "Des erreurs se sont produites lors de la suppression : <br />";
+                    errors.forEach(function(elt) {
+                        msg += elt.message +" <br />";
+                    });
+                    ServiceAlert.addError(msg);
+                }
+                // Si tous les retours sont en succès => 1 seul message OK
+                else {
+                    // Le bien a été supprimé du système du partenaire => change a valeur du boolean "exist" à false
+                    $scope.partners.filter(function(elt) {return elt.name == partner}).forEach(function(elt) {
+                        elt.exist = false;
+                    })
+                    ServiceAlert.addSuccess("Suppression réalisée avec succès");
+                }
 
             });
     }
