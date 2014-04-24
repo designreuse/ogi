c023d7eb2fb795322eaa34f3fb514b9d40386fb6
jerep6 <jerep6@gmail.com>
2014-04-24 13:50:42 +0200

    Turn into variable image dpe url

--- ogi-parent/ogi-web/src/main/webapp/js/module/property/controller/ControllerPrpTabDiagnosis.js
+++ ogi-parent/ogi-web/src/main/webapp/js/module/property/controller/ControllerPrpTabDiagnosis.js
@@ -1,5 +1,7 @@
 function ControllerPrpTabDiagnosis($scope, Page, $routeParams, ServiceConfiguration, ServiceDPE) {
 
+    $scope.API_URL = ServiceConfiguration.API_URL;
+
     // Watch change for dpe values to update classifications
     $scope.$watch('prp.dpe.kwh', function(newVal, oldVal) {
         $scope.prp.dpe.classificationKWh = ServiceDPE.getKWHClassifiation(newVal);
---
+++ ogi-parent/ogi-web/src/main/webapp/js/module/property/service/ServicesProperty.js
@@ -0,0 +1,40 @@
+myApp.factory('ServiceDPE', function(Utils) {
+    var dpeConfigKWH = [
+        {min: 0, classification: "A"},
+        {min: 51, classification: "B"},
+        {min: 91, classification: "C"},
+        {min: 151, classification: "D"},
+        {min: 231, classification: "E"},
+        {min: 331, classification: "F"},
+        {min: 451, classification: "G"}
+    ];
+    var dpeConfigGes = [
+        {min: 0, classification: "A"},
+        {min: 6, classification: "B"},
+        {min: 11, classification: "C"},
+        {min: 21, classification: "D"},
+        {min: 36, classification: "E"},
+        {min: 56, classification: "F"},
+        {min: 81, classification: "G"}
+    ];
+
+    var getClassification = function(tab, dpeValue) {
+        if(Utils.isUndefinedOrNull(dpeValue)) {return "";}
+
+        var conf = tab[0];
+        for(var i = 0; i < tab.length; i++) {
+            if (dpeValue >= tab[i].min) {
+                conf = tab[i];
+            }
+        }
+        return conf.classification;
+    }
+    return {
+        getKWHClassifiation : function(dpeValue){
+            return getClassification(dpeConfigKWH, dpeValue);
+        },
+        getGesClassifiation : function(dpeValue){
+            return getClassification(dpeConfigGes, dpeValue);
+        }
+    }
+});
