b359a89cc2c455ed1c78eafe64315aa2003c6890
jerep6 <jerep6@gmail.com>
2014-06-22 11:32:30 +0200

    Display documents

--- ogi-parent/ogi-web/src/main/webapp/js/module/property/controller/ControllerPrpTabDocuments.js
+++ ogi-parent/ogi-web/src/main/webapp/js/module/property/controller/ControllerPrpTabDocuments.js
@@ -1,7 +1,30 @@
-function ControllerPrpTabDocuments($scope, Page, $routeParams, ServiceConfiguration, ServiceObject, ServiceAlert, $http, $log ,$modal) {
+function ControllerPrpTabDocuments($scope, Page, $routeParams, ServiceConfiguration, ServiceAlert, $http, $log) {
 
+    $scope.documents = [
+        {
+            "name" : "Fiche Vitrine",
+            "type" : "vitrine",
+            "availablePageSize" : ["A4", "A3"],
+            "selectedPageSize" : "A4"
+        },
+        {
+            "name" : "Fiche Classeur",
+            "type" : "classeur",
+            "availablePageSize" : ["A4"],
+            "selectedPageSize" : "A4"
+        }
+    ]
 
     $scope.httpGetCurrentType.success(function() {
         $scope.linkShopFront = ServiceConfiguration.API_URL+"/rest/report/"+$scope.prp.reference;
     });
+
+    $scope.computeLink = function (doc, format) {
+        var link = $scope.linkShopFront+"?format="+format+"&type="+doc.type+"&pageSize="+doc.selectedPageSize;
+        window.location.href=link;
+    }
+
+    $scope.displayPageSize = function(doc) {
+        return doc.availablePageSize.length > 1;
+    }
 }
