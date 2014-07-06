f125b1879e92a1eff115ca3d91d10f054bb7c4f3
jerep6 <jerep6@gmail.com>
2014-07-06 17:13:58 +0200

    Documents preview

--- ogi-parent/ogi-web/src/main/webapp/js/module/property/controller/ControllerPrpTabDocuments.js
+++ ogi-parent/ogi-web/src/main/webapp/js/module/property/controller/ControllerPrpTabDocuments.js
@@ -1,20 +1,27 @@
 function ControllerPrpTabDocuments($scope, Page, $routeParams, ServiceConfiguration, ServiceAlert, $http, $log) {
+    $scope.availableFormats = {
+        "docx" : {"title" : "Télécharger au format Word", "img": "img/logo-word.png"},
+        "odt" : {"title" : "Télécharger au format Livre Office", "img": "img/logo-writer.png"},
+        "pdf" : {"title" : "Télécharger au format PDF", "img": "img/logo-pdf.png"}
+    }
 
     $scope.documents = [
         {
             "name" : "Fiche Vitrine",
             "type" : "vitrine",
             "availablePageSize" : ["A4", "A3"],
-            "selectedPageSize" : "A4"
-        }
-        /*
+            "selectedPageSize" : "A4",
+            "formats": ["pdf", "docx", "odt"]
+        },
+
         {
-            "name" : "Fiche Classeur",
-            "type" : "classeur",
+            "name" : "Fiche Client",
+            "type" : "client",
             "availablePageSize" : ["A4"],
-            "selectedPageSize" : "A4"
+            "selectedPageSize" : "A4",
+            "formats": ["pdf"]
         }
-        */
+
     ]
 
     $scope.httpGetCurrentType.success(function() {
