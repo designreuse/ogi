5b8bdf872efec37e2db2777c4db4a9d50ffdb214
jerep6 <jerep6@gmail.com>
2014-07-26 10:16:55 +0200

    Handle custom http status on exception

--- ogi-parent/ogi-ws/src/main/java/fr/jerep6/ogi/rest/provider/GlobalExceptionHandler.java
+++ ogi-parent/ogi-ws/src/main/java/fr/jerep6/ogi/rest/provider/GlobalExceptionHandler.java
@@ -5,12 +5,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import fr.jerep6.ogi.framework.exception.BusinessException;
 import fr.jerep6.ogi.framework.exception.MultipleBusinessException;
 import fr.jerep6.ogi.framework.transfert.ExceptionTo;
 import fr.jerep6.ogi.transfert.mapping.OrikaMapper;
@@ -23,14 +26,17 @@ public class GlobalExceptionHandler {
 	@Autowired
 	private OrikaMapper	mapper;
 
-	@ExceptionHandler({ Exception.class, MultipleBusinessException.class })
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public ExceptionTo defaultErrorHandler(HttpServletRequest req, Exception e) {
+	@ExceptionHandler({ Exception.class, BusinessException.class, MultipleBusinessException.class })
+	public ResponseEntity<ExceptionTo> defaultErrorHandler(HttpServletRequest req, Exception e) {
 		LOGGER.error("exception", e);
+
+		// Get response status from exeption class
+		ResponseStatus responseStatus = AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class);
 
 		// Convert exception into light object
 		ExceptionTo error = mapper.map(e, ExceptionTo.class);
-		return error;
+		ResponseEntity<ExceptionTo> r = new ResponseEntity<ExceptionTo>(error,
+				responseStatus != null ? responseStatus.value() : HttpStatus.INTERNAL_SERVER_ERROR);
+		return r;
 	}
-
 }
