7db76f47d80cf35ead0b400bf09a96f99eb2f0ea
jerep6 <jerep6@gmail.com>
2014-10-26 00:00:45 +0200

    Change Dockerfile to work with to volume container

--- docker/OGIBase/run.sh
+++ docker/OGIBase/run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+#Create data structure
+
+mkdir -p /data/tmp
+mkdir -p /data/storage
+mkdir -p /data/logs
+
 #Run mysql
-mysqld_safe &
+/app/bin/run-mysql.sh
 
 # Run tomcat
-/app/tomcat/bin/startup.sh
+#/app/tomcat/bin/startup.sh
+/app/tomcat/bin/catalina.sh run
