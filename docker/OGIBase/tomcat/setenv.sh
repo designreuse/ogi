0c43d73e3cce1536c92aac433cc2276d43a7d816
jerep6 <jerep6@gmail.com>
2015-07-05 00:00:23 +0200

    Utilisation d'une variable globale pour la clé

--- docker/OGIBase/tomcat/setenv.sh
+++ docker/OGIBase/tomcat/setenv.sh
@@ -1,3 +1,4 @@
 JAVA_HOME=/app/java/jdk/
 CATALINA_PID="$CATALINA_BASE/tomcat.pid"
-CATALINA_OPTS="-Xms256M -Xmx400M -DpropertyConfigurerPassword=\"OGI_KEY\""
+CATALINA_OPTS="-Xms256M -Xmx400M -DpropertyConfigurerPassword=\"$OGI_KEY\""
+
