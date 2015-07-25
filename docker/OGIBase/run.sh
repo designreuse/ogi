7a9149bfe921180029cfdc9829a930652fa88854
jerep6 <jerep6@gmail.com>
2015-07-26 00:04:24 +0200

    Run ogi with NON root user

--- docker/OGIBase/run.sh
+++ docker/OGIBase/run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-#Run mysql
+# Create ogi structure into data directory. Volume is mounted at run
+mkdir -p /data/ && chown -R ogi:ogi /data/
+su - ogi -c "mkdir -p /data/tmp && mkdir -p /data/ogi/storage && mkdir -p /data/elasticsearch && mkdir -p /data/backup && mkdir -p /data/mysql"
+
+# Run mysql
 /etc/init.d/mysql start
 
 # Run tomcat
 #/app/tomcat/bin/startup.sh
-/app/tomcat/bin/catalina.sh run
+su - ogi -c "/app/tomcat/bin/catalina.sh run"
